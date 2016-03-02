d581e38f05fc3d42b2a381340a06985d05b3d381
abt8WG <abt8WG@live.jp>
2016-03-02 18:49:22 +0900

    git の revision を埋め込んでみる
    Git Shell 上から起動した Visual Studio で Release 版をビルドするときのみ
    各ファイルの「ファイルの説明」に revision hash を付加する
    
    BonCtrl や Common の更新は反映してない

---
+++ EpgTimer/EpgTimer/Revision.cs
@@ -0,0 +1,1 @@
+namespace EpgTimer{class Revision{public const string GIT_REVISION="";}} 
---
+++ bin/pre-build.cmd
@@ -0,0 +1,13 @@
+@echo off
+if "%2" == "" goto :eof
+setlocal
+set gitcmd="%github_git%\cmd\git.exe"
+set REV=""
+if /i "%1" == "Release" if exist "%gitcmd%" for /f %%a in ('"%gitcmd% log -1 --format=%%h %2"') do set REV=" (%%a)"
+if exist %2Revision.cs (
+  echo update %2Revision.cs - %REV%
+  echo namespace EpgTimer{class Revision{public const string GIT_REVISION=%REV%;}} > %2Revision.cs
+) else (
+  echo update %2gitrevision.h - %REV%
+  echo #define GIT_REVISION %REV% > %2gitrevision.h
+)
