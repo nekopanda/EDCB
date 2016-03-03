905f5de3f882dc9b258f9d9bd13f98f19f1141d3
abt8WG <abt8WG@live.jp>
2016-03-03 21:33:59 +0900

    EpgTimer / ポップアップ表示を見切れないようにする

--- EpgTimer/EpgTimer/TunerReserveViewCtrl/TunerReserveView.xaml.cs
+++ EpgTimer/EpgTimer/TunerReserveViewCtrl/TunerReserveView.xaml.cs
@@ -63,10 +63,16 @@ namespace EpgTimer.TunerReserveViewCtrl
             
             popupItem.Background = viewInfo.BackColorTuner;
 
-            Canvas.SetLeft(popupItem, Math.Floor(viewInfo.LeftPos));
-            Canvas.SetTop(popupItem, Math.Floor(viewInfo.TopPos));
-            popupItem.Width = Math.Ceiling(viewInfo.Width + 1);
-            popupItem.MinHeight = Math.Ceiling(viewInfo.Height + 1);
+            UpdatePopupPosition(viewInfo.LeftPos, viewInfo.TopPos);
+            popupItem.Width = viewInfo.Width;
+            if (viewInfo.TopPos < scroll.ContentVerticalOffset)
+            {
+                popupItem.MinHeight = viewInfo.TopPos + viewInfo.Height - scroll.ContentVerticalOffset;
+            }
+            else
+            {
+                popupItem.MinHeight = Math.Min(scroll.ContentVerticalOffset + scroll.ActualHeight - viewInfo.TopPos - 18, viewInfo.Height);
+            }
             //popupItemTextArea.Margin = new Thickness(2, -1, 0, 0);
             popupItemTextArea.Margin = new Thickness(1, -1, 5, 1);
 
