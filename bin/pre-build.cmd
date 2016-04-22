4de0e897d15e3814ad605f6a5d19505e61a20a0b
abt8WG <abt8WG@live.jp>
2016-04-22 22:59:01 +0900

    build / NAS 上に clone してもビルドできるように変更
    あわせて git コマンドを PATH から探すように変更
    またビルドエラーでトラブる人がでるかも…

--- bin/pre-build.cmd
+++ bin/pre-build.cmd
@@ -1,13 +1,23 @@
 @echo off
-if (%2) == () goto :eof
 setlocal
-set gitcmd="%github_git%\cmd\git.exe"
+if (%2) == () goto :eof
+if not exist %2 goto :eof
+
+@rem git.exe ‚ðŒ©‚Â‚¯‚é
+set gitcmd=
+if /i (%1) == (Release) for /f %%a in ('where /f git.exe') do set gitcmd=%%a
+
+@rem revision ‚ðŽæ“¾‚·‚é
 set REV=""
-if /i (%1) == (Release) if exist %gitcmd% for /f %%a in ('"%gitcmd% log -1 --format=%%h "%~f2.""') do set REV=" (%%a)"
-if exist %2Revision.cs (
-  echo update %2Revision.cs - %REV%
-  echo namespace EpgTimer{class Revision{public const string GIT_REVISION=%REV%;}} > %2Revision.cs
+pushd %2
+if not (%gitcmd%) == () for /f %%a in ('"%gitcmd% log -1 --format=%%h ."') do set REV=" (%%a)"
+popd
+
+@rem revision header file ‚ð¶¬‚·‚é
+if exist %2\Revision.cs (
+  echo update %2\Revision.cs - %REV%
+  echo namespace EpgTimer{class Revision{public const string GIT_REVISION=%REV%;}} > %2\Revision.cs
 ) else (
-  echo update %2gitrevision.h - %REV%
-  echo #define GIT_REVISION %REV% > %2gitrevision.h
+  echo update %2\gitrevision.h - %REV%
+  echo #define GIT_REVISION %REV% > %2\gitrevision.h
 )
